7430d576504809af12e3cfd96033548bcc86110d
truongkhanhduy95 <truongkhanhduy9@gmail.com>
2018-07-17 15:50:24 +0700

    Add extension

--- Unlock/ActionUnlock.cs
+++ Unlock/ActionUnlock.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using Android.App.Job;
+using Android.Content;
+using Java.Lang;
+
 namespace Unlock
 {
-    public class ActionUnlock
+    public static class ActionUnlock
     {
         public const string Recall = "Android.Content.Intent.ActionScreenOff.Recall";
+
+        public static ComponentName GetComponentNameForJob<T>(this Context context) where T : JobService, new()
+        {
+            Class javaClass = Class.FromType(typeof(T));
+            return new ComponentName(context, javaClass);
+        }
     }
 
 }
